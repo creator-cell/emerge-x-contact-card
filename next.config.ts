642bef2bb912350679ead94c5ebb11e03ee89b42
Creator-cell <55391423+creator-cell@users.noreply.github.com>
2025-04-30 23:52:09 +0530

    Update next.config.ts
    updated link

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   env: {
-    SERVER: process.env.NODE_ENV === 'production' ? 'http://3.29.44.158:3000/v1' : 'http://localhost:8081/v1',
+    SERVER: process.env.NODE_ENV === 'production' ? 'https://web-api.emerge-x.com/v1' : 'http://localhost:8081/v1',
     GOOGLE_API_KEY: 'AIzaSyAT-a4KE6-4BsnGdk_zdZmgWqwd_oAnGQg'
   }
 };
